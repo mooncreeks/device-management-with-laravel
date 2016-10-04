651cea4ddd3780deff523555eff2a67aefb653b9
turjoy <sayed.zayeem@gagagugu.com>
2016-10-04 15:08:43 +0600

    started refactoring: makingde User folder for TeamsController and PersonsController and moved those controllers

--- app/Http/Controllers/User/PersonsController.php
+++ app/Http/Controllers/User/PersonsController.php
@@ -1,6 +1,7 @@
 <?php
+namespace App\Http\Controllers\User;
 
-namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,35 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-//Route::get('/', 'SearchesController@search');
-
 Route::auth();
-
-//Route::get('/home', 'HomeController@index');
 
 Route::resource('devices', 'DevicesController');
 
-Route::resource('persons', 'PersonsController');
+Route::resource('persons', 'User\PersonsController');
 
-Route::resource('teams', 'TeamsController');
-
-Route::any('search', ['as'=>'searches.index', 'uses'=>'SearchesController@search'] );
-
-//Route::resource('queries', 'QueryController');
-// Route::controller('datatables', 'DatatablesController', [
-//     'anyData'  => 'datatables.data',
-//     'getIndex' => 'datatables',
-// ]);
+Route::resource('teams', 'User\TeamsController');
 
 Route::get('/', 'DatatablesController@getIndex');
 
